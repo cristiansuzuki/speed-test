a0cc1906002b0291ce2024b15fae469f0f159f2e
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2021-06-25 11:57:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ melhor_server = teste.get_best_server() # Pega o melhor servidor
 print(f"Encontrado: {melhor_server['host']} localizado em {melhor_server['country']}")
  
 print("Realizando teste de download...")
-download = teste.download()
+download = teste.download() # realiza o reste de download
 
-print("Realizando teste de upload...")
-upload = teste.upload()
+print("Realizando teste de upload...") 
+upload = teste.upload() # realiza o teste de upload
 
-ping = teste.results.ping
+ping = teste.results.ping # realiza teste de ping
 
+# Printa no terminal e formata os dados 
 print(f"Velocidade de download: {download / 1024 / 1024:.2f} Mb/s")
 print(f"Velocidade de upload: {upload / 1024 / 1024:.2f} Mb/s")
 print(f"Ping: {ping:.2f} ms")
